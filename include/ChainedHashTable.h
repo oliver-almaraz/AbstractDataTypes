2ba9ef83e616a9d7b896fb18c502abcc2a01e07e
oliver-almaraz <oliver.almaraz@gmail.com>
2022-06-11 21:03:53 -0500

    Initial commit of ChainedHashTable.h & ChainedHashTable.c

---
+++ include/ChainedHashTable.h
@@ -0,0 +1,97 @@
+/******************************************************
+*                ChainedHashTable.h
+*	Created & mantained by: oliver.almaraz@gmail.com
+*	for personal use. You may use and modify it.
+*	github.com/oliver-almaraz/AbstractDataTypes
+*******************************************************
+*
+*	C library providing an interface for creating and
+*	destroying a Chained Hash Table, as well as for
+*	inserting, removing and searching elements in it.
+*
+*	This library is an EXTENSION of the LinkedList
+*	lib provided in this repository. Therefore, you
+*	should be aware of the description presented in
+*	./LinkedList.h. For even more detail, you may look
+*	into ./source/LinkedList.c.
+*
+*	chtable_alloc() allocates space for the structure
+*	of the data structure which is freed when calling
+*	chtable_destroy().
+*
+*	chtable_init() takes a destroy_data() function
+*	that will be passed to every bucket for freeing
+*	the memory allocated for every Linked List.
+*
+*	chtable_lookup() calls the MATCH function passed
+*	to chtable_init() to compare its second argument
+*	with the data contained in the BUCKET's (L. List)
+*	element (node). If the element is found in the
+*	TABLE, chtable_lookup returns a pointer to the
+*	DATA element of the node (list->node->data).
+*	A common scenario is to store structs as the DATA
+*	in the NODES and have a MATCH fn that compares
+*	only one member of that struct: you search for
+*	one member of the struct and you get a pointer
+*	to the whole struct with chtable_lookup()
+*
+********************************************************
+*	chtable_remove() DESTROYS the element passed to it
+*	and frees the allocated mem for it. You cannot
+*	access a table element after you passed it to
+*	chtable_remove()!
+********************************************************
+*
+*	User has to choose appropriate functions:
+*	(examples are provided in this repository)
+*		int hash_fn(void *key)
+*		int match(void *key1, void *key2)
+*		void destroy(void *data)
+*	Also, user must decide a suitable number of
+*	buckets (see ./HashFunction_djb2.h for suggestions)
+*/
+
+#ifndef CHAINED_HASH_TABLE
+#define CHAINED_HASH_TABLE
+
+#include <stdlib.h>
+#include "LinkedList.h"
+
+// Structure  for the Chained Hash TABLE
+typedef struct ChainedHashTable_ {
+	List *table; // The array of buckets
+
+	int buckets; // Number of buckets (L. Lists) in TABLE
+	int size; // Number of elements
+
+	// Hash function
+	int (*hash_fn)(void *key);
+	// Match function
+	int (*match)(void *key1, void *key2);
+
+	// Destroy function
+	// It's the destroy_data function that will be
+	// passed to each bucket (L. List) to destroy the
+	// NODE's DATA. (See LinkedList.h)
+	void (*destroy_data)(void *data);
+} ChainedHashTable;
+
+/***********************************************
+*               PUBLIC INTERFACE
+***********************************************/
+
+#define chtable_alloc() (ChainedHashTable*)malloc(sizeof(ChainedHashTable))
+
+int chtable_init(
+	ChainedHashTable *ch_table, int buckets,
+	int (*hash_fn)(void *key),
+	int (*match)(void *key1, void *key2),
+	void (*destroy)(void *data)
+);
+
+void* chtable_lookup(ChainedHashTable *ch_table, void *data);
+int chtable_insert(ChainedHashTable *ch_table, void *data);
+int chtable_remove(ChainedHashTable *ch_table, void *data);
+void chtable_destroy(ChainedHashTable *ch_table);
+
+#endif
---
+++ source/ChainedHashTable.c
@@ -0,0 +1,140 @@
+/******************************************************
+*                ChainedHashTable.c
+*	Created & mantained by: oliver.almaraz@gmail.com
+*	for personal use. You may use and modify it.
+*	github.com/oliver-almaraz/AbstractDataTypes
+*******************************************************/
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "LinkedList.h"
+#include "ChainedHashTable.h"
+
+int chtable_init (
+	ChainedHashTable *ch_table, int buckets,
+	int (*hash_fn)(void *key),
+	int (*match)( void *key1, void *key2),
+	void (*destroy_data)(void *data)
+)
+{
+	// Allocate contiguous memory for the table
+	// (array of linked lists)
+	ch_table->table = (List*)malloc(buckets * sizeof(List));
+	if (ch_table->table == NULL) return -1;
+
+	// Number of buckets in table
+	ch_table->buckets = buckets;
+
+	// Initialize the buckets in table
+	for (int i=0; i < buckets; i++){
+		// We can perform pointer arithmetic because
+		// we allocated contiguous mem for _table_
+		List *bucket = ch_table->table + i;
+		list_init(bucket, destroy_data);
+	}
+
+	ch_table->hash_fn = hash_fn;
+	ch_table->match = match;
+	ch_table->destroy_data = destroy_data;
+
+	ch_table->size = 0;
+
+	return 0;
+}
+
+void* chtable_lookup(ChainedHashTable *ch_table, void *data){
+	// Calculate to what bucket DATA belongs
+	int bucket = ch_table->hash_fn(data) % ch_table->buckets;
+	// Get a pointer to that bucket
+	// ("&" is required because array[] notation dereferences)
+	List *bucketList = &ch_table->table[bucket];
+
+	for (
+		// Start at the HEAD of the bucket:
+		Node *element =  bucketList->head;
+		// Until the tail is reached
+		element != NULL;
+		element = element->next
+	)
+	{
+		if (ch_table->match(data, element->data)){
+			// Element found, return ptr to data
+			return element->data;
+		}
+	}
+	// Not found
+	return NULL;
+}
+
+int chtable_insert(ChainedHashTable *ch_table, void *data){
+	// Check if DATA is already in the table
+	if (chtable_lookup(ch_table, data) != NULL)
+		return 1;
+
+	int bucket =  ch_table->hash_fn(data) % ch_table->buckets;
+
+	int retval = list_add_head(&ch_table->table[bucket], data);
+	if (retval == 0)
+		ch_table->size++;
+
+	// retuns -1 if it could not allocate the elmt
+	return retval;
+}
+
+int chtable_remove(ChainedHashTable *ch_table, void *data){
+	// Calculate to what bucket DATA belongs
+	int bucket = ch_table->hash_fn(data) % ch_table->buckets;
+
+	// Get a pointer to that bucket
+	// ("&" is required because array[] notation dereferences)
+	List *bucketList = &ch_table->table[bucket];
+
+	// If the node to remove is not at the HEAD,
+	// we'll list_rem_next using this prev ptr.
+	Node *prev;
+
+	for (
+		// Start at the HEAD of the bucket:
+		Node *element =  bucketList->head;
+		// Until the tail is reached
+		element != NULL;
+		element = element->next
+	)
+	{
+		if (ch_table->match(data, element->data)){
+			// Element found, remove it
+
+			// Check if it's the list's HEAD
+			if (element == bucketList->head){
+				int retval = list_rem_head(bucketList);
+				if (retval == 0){
+					ch_table->size--;
+					return 0;
+				}
+			} else {
+				// Element it elsewhere in the list
+				int retval = list_rem_next(bucketList, prev);
+				if (retval == 0){
+					ch_table->size--;
+					return 0;
+				}
+			}
+		} else
+		// Save this ptr for list_rem_next if the
+		// elmt is found in the next iteration
+		prev = element;
+	}
+	// Loop ended, element not found
+	return -1;
+}
+
+void chtable_destroy(ChainedHashTable *ch_table){
+	for (int i=0; 1 < ch_table->buckets; i++)
+		list_destroy(&ch_table->table[i]);
+	// Mem for the buckets was allocated contigu.
+	// and is now freed as a single pointer
+	free(ch_table->table);
+	memset(ch_table, 0, sizeof(ChainedHashTable));
+	free(ch_table);
+}
